623711f90d838987958ecf8a7cbcdae971ca0998
João Vitor <joaovitorfelixcontato@gmail.com>
2025-05-08 22:14:30 -0300

    feat: add active link styles to Navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">
 import logo from '@/assets/logo.svg'
-import { RouterLink } from 'vue-router'
+import { RouterLink, useRoute } from 'vue-router'
+
+const route = useRoute()
+
+function isPathActive(path: string) {
+  return route.path === path
+}
 </script>
 
 <template>
@@ -8,25 +14,25 @@ import { RouterLink } from 'vue-router'
     <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
       <div class="flex h-20 items-center justify-between">
         <div class="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
-          <a class="flex flex-shrink-0 items-center mr-4" href="/">
+          <RouterLink class="flex flex-shrink-0 items-center mr-4" to="/">
             <img class="h-10 w-auto" :src="logo" alt="Vue Jobs" />
             <span class="hidden md:block text-white text-2xl font-bold ml-2">Vue Jobs</span>
-          </a>
+          </RouterLink>
           <div class="md:ml-auto">
             <div class="flex space-x-2">
               <RouterLink
                 to="/"
-                class="text-white bg-green-900 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                :class="`text-white  rounded-md px-3 py-2 ${isPathActive('/') ? 'bg-green-900' : 'hover:bg-gray-900 hover:text-white'}`"
                 >Home</RouterLink
               >
               <RouterLink
                 to="/jobs"
-                class="text-white hover:bg-green-900 hover:text-white rounded-md px-3 py-2"
+                :class="`text-white rounded-md px-3 py-2 ${isPathActive('/jobs') ? 'bg-green-900' : 'hover:bg-gray-900 hover:text-white'}`"
                 >Jobs</RouterLink
               >
               <RouterLink
                 to="/jobs/add"
-                class="text-white hover:bg-green-900 hover:text-white rounded-md px-3 py-2"
+                :class="`text-white rounded-md px-3 py-2 ${isPathActive('/jobs/add') ? 'bg-green-900' : 'hover:bg-gray-900 hover:text-white'}`"
                 >Add Job</RouterLink
               >
             </div>
