4703b5289a91db7529127e08f416675d7d4d7d1e
João Vitor <joaovitorfelixcontato@gmail.com>
2025-05-08 21:36:08 -0300

    chore: add primeicons

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import 'primeicons/primeicons.css'
 import './assets/main.css'
 
 import { createApp } from 'vue'
@@ -5,7 +6,5 @@ import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-
 app.use(router)
-
 app.mount('#app')
